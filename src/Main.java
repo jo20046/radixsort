596c6f36506912edb7b2f8ba9a1af0467ed58191
Enthöfer Joshua <j.enthoefer@laudert.de>
2019-10-09 16:11:24 +0200

    Radixsort

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,58 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] sequence = {47, 85, 10, 45, 16, 34, 67, 80, 34, 4, 0, 99};
+//        int[] sequence = {47, 85, 10, 45, 16, 34, 67, 80, 34, 4, 0, 99};
+//        System.out.println("Original: " + Arrays.toString(sequence));
+//        bucketsort(sequence, 10);
+//        System.out.println("Sortiert: " + Arrays.toString(sequence));
+
+
+        int[] sequence = {124, 523, 483, 128, 923, 584};
         System.out.println("Original: " + Arrays.toString(sequence));
-        bucketsort(sequence, 10);
+        radixsort(sequence, 3, 10);
         System.out.println("Sortiert: " + Arrays.toString(sequence));
 
+    }
+
+    private static void radixsort(int[] array, int len, int keys) {
+
+        //Init Buckets
+        List<Integer>[] buckets = new List[keys];
+        for (int i = 0; i < keys; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        // Sorting
+        for (int exp = 0; exp < len; exp++) {
+
+            clearBuckets(buckets);
+
+            // Partitioning
+            for (int e : array) {
+                buckets[hash(e, exp)].add(e);
+            }
+
+            // Collecting
+            int i = 0;
+            for (List<Integer> bucket : buckets) {
+                for (Integer e : bucket) {
+                    array[i++] = e;
+                }
+            }
+            System.out.println("Array nach Exp " + exp + ": " + Arrays.toString(array));
+        }
+
+
+    }
+
+    private static int hash(int num, int exp) {
+        return (num / (int)Math.pow(10, exp)) % 10;
+    }
+
+    private static void clearBuckets(List<Integer>[] buckets) {
+        for (List<Integer> bucket : buckets) {
+            bucket.clear();
+        }
     }
 
     private static void bucketsort(int[] array, int k) {
